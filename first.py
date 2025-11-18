4c42646663d022c8c48a755bfa0d1543ce0b83de
carteryee7 <carter.yee07@gmail.com>
2025-11-18 15:22:42 +0200

    lkj

--- first.py
+++ first.py
@@ -1,6 +1,19 @@
-print('hello world')
-
-array = [1, 'truck']
-print(array)
-
-print(array)
+class Food():
+    def __init__(self, name, rating, type, cuisine):
+        self.name = name
+        self._rating = rating
+        self.type = type
+        self.cuisine = cuisine
+    
+    @property
+    def rating(self):
+        return self._rating
+    
+    @rating.setter
+    def rating(self, value):
+        if 0 <= value <= 10:
+            self._rating = value
+        else:
+            raise ValueError("Rating must be between 0 and 5")
+    
+    
