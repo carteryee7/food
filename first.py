16d8ee39a60b93fd8e3c26b7b4197b147e6ba826
carteryee7 <carter.yee07@gmail.com>
2025-11-18 15:29:19 +0200

    asdf

--- first.py
+++ first.py
@@ -26,6 +26,9 @@ class Ranker():
     
     def add_food(self, food):
         self.foods.append(food)
+
+    def remove_food(self, food_name):
+        self.foods = [food for food in self.foods if food.name != food_name]
     
     def sort_by_rating(self):
         return sorted(self.foods, key=lambda x: x.rating, reverse=True)
